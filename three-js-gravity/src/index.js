b7a704cb14a1a6688169faeac95354477d322093
Timothée Gauthier <t.gauthier42@gmail.com>
2018-12-19 23:22:16 +0100

    Orbit sphere + control camera

--- three-js-gravity/src/index.js
+++ three-js-gravity/src/index.js
@@ -1,6 +1,6 @@
 const THREE = require('three');
+require('./OrbitControls');
 
-const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -10,45 +10,93 @@ document.body.appendChild(renderer.domElement);
 camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.gammaInput = true;
+renderer.gammaOutput = true;
+
 function createCube() {
   const geometryBox = new THREE.BoxGeometry(20, 20, 20);
-  const materialMesh = new THREE.MeshBasicMaterial({ color: 0x0ff00f });
+  const materialMesh = new THREE.MeshLambertMaterial({ color: 0x0ff00f });
   return new THREE.Mesh(geometryBox, materialMesh);
 }
 
 function createLine() {
-  const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 21 });
+  const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
   const geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-  geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(-20, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 20, 0));
+  geometry.vertices.push(new THREE.Vector3(20, 0, 0));
   return new THREE.Line(geometry, materialLine);
 }
 
 function createSphere() {
-  const geometry = new THREE.SphereGeometry(40, 40, 30, 40);
-  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const geometry = new THREE.SphereGeometry(20, 20, 20, 20);
+  const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
   return new THREE.Mesh(geometry, material);
 }
 
 const cube = createCube();
 const line = createLine();
 const sphere = createSphere();
-sphere.position.set(-30, 0, 0)
 
-// const light = new THREE.AmbientLight(0xffffff);
-// scene.add(light);
+sphere.position.set(-70, 0, 0);
+
+// var directionalLight = new THREE.DirectionalLight( 0xffffff, 0 );
+// scene.add( directionalLight );
+const controls = new THREE.OrbitControls(camera);
+
+
+const scene = new THREE.Scene();
+const ambient = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambient);
+const spotLight = new THREE.SpotLight(0xffffff, 1);
+spotLight.position.set(15, 40, 35);
+spotLight.angle = Math.PI / 4;
+spotLight.penumbra = 0.05;
+spotLight.decay = 2;
+spotLight.distance = 200;
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 10;
+spotLight.shadow.camera.far = 200;
+scene.add(spotLight);
+
+const lightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(lightHelper);
+const shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+scene.add(shadowCameraHelper);
+
+
 scene.add(cube);
 scene.add(line);
 scene.add(sphere);
 
+const geometry = new THREE.PlaneGeometry(100, 20, 3);
+const material = new THREE.MeshBasicMaterial({ color: 0x0e0e0e, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01 / 2;
-  cube.rotation.y += 0.01 / 2;
+  controls.update();
 
-  line.rotation.x += 0.1 / 2;
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.005;
+
+  line.rotation.x += 0.05;
   line.rotation.y += 0.001;
+
+  const orbitRadius = 100; // for example
+  const date = Date.now() * 0.001;
+
+  sphere.position.set(
+    Math.cos(date) * orbitRadius,
+    0,
+    Math.sin(date) * orbitRadius,
+  );
+
   renderer.render(scene, camera);
 }
 
